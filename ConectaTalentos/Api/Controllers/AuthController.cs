d2b9de8de75767dc5ed9d7840fb6fd7b89efe133
Arthursouzafut22 <arthursouzafut22@gmail.com>
2026-08-07 13:30:45 -0300

    first commit

---
+++ ConectaTalentos/Api/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConectaTalentos.Api.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> register()
+        {
+            return Ok();
+        }
+    }
+}
